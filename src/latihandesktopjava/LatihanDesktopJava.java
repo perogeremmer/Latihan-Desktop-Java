bc78b5f4b50eff241afb321b328eb733fbcde01f
Hudya <mhudyaramadhana@gmail.com>
2022-04-07 14:38:22 +0700

    First Commit

---
+++ src/latihandesktopjava/LatihanDesktopJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihandesktopjava;
+
+import form.CobaFrame;
+
+
+/**
+ *
+ * @author kiddy
+ */
+public class LatihanDesktopJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CobaFrame frame = new CobaFrame();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+}
